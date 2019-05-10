f971768dc007f596e89c2d002e39ea295837cb3e
Wing Him Liu <winghim@dataking.co.kr>
2019-05-10 16:48:53 +0900

    export all the missing new functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
+import format from "./format";
 import formDataObject from "./formDataObject";
+import ifElseValue from "./ifElseValue";
+import instanceOf from "./instanceOf";
 import method from "./method";
+import notNilThen from "./notNilThen";
 import safelyApply from "./safelyApply";
 import submitEventData from "./submitEventData";
 import validateEmail from "./validateEmail";
 
 const module = {
+    format,
     formDataObject,
+    ifElseValue,
+    instanceOf,
     method,
+    notNilThen,
     safelyApply,
     submitEventData,
     validateEmail,
@@ -14,8 +22,12 @@ const module = {
 
 window.dkjs = module;
 
+export { default as format } from "./format";
 export { default as formDataObject } from "./formDataObject";
+export { default as ifElseValue } from "./ifElseValue";
+export { default as instanceOf } from "./instanceOf";
 export { default as method } from "./method";
+export { default as notNilThen } from "./notNilThen";
 export { default as safelyApply } from "./safelyApply";
 export { default as submitEventData } from "./submitEventData";
 export { default as validateEmail } from "./validateEmail";
